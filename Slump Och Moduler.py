3fa5bad37e82124ca4bfa5bc27199cfd93c4a071
Alexsander <alelin36@edu.umea.se>
2026-09-03 13:44:20 +0200

    lektion. 3 seprtember

--- Slump Och Moduler.py
+++ Slump Och Moduler.py
@@ -5,16 +5,52 @@ player2 = input("player 2")
 carddraw = random.randint(1,11)
 carddraw3 = random.randint(1,11)
 carddraw5 = random.randint(1,11)
-print ((player1)+ str(carddraw))
+carddraw7 = random.randint(1,11)
+print ((player1)+str("-")+ str(carddraw)+str(-carddraw3))
+print (carddraw+carddraw3)
 carddraw2 = random.randint(1,11)
 carddraw4 = random.randint(1,11)
 carddraw6 = random.randint(1,11)
-print((player2)+ str(carddraw2))
+carddraw8 = random.randint(1,11)
+print((player2)+str("-")+ str(carddraw2)+str(-carddraw4))
+print(carddraw2+carddraw4)
 
 player100 = input("hit or stand")
 if player100 == "hit": 
-    print (player1+str(carddraw3+carddraw))
+    print (player1+str(carddraw5)+str(-carddraw)+str(-carddraw3))
+    print(carddraw+carddraw3+carddraw5)
 else: 
-    print (player100+ str(carddraw))
+    print (player1+ str(-carddraw+carddraw3))
+
 
 player200 = input("hit or stand")
+if player200 == "hit":
+    print(player2+str(carddraw6)+str(-carddraw2)+str(-carddraw4))
+    print(carddraw2+carddraw4+carddraw6)
+else:
+    print(player2+str(-carddraw2+-carddraw4))
+
+if player100 == "stand":
+    print(player1+ str(-carddraw+carddraw3))
+else:
+    player1000 = input ("hit or stand")
+
+    if player1000 == "hit":
+        print (player1+ "-du har korten"+str(-carddraw)+str(-carddraw3)+str(-carddraw5)+str(-carddraw7))
+        print((carddraw)+(carddraw3)+(carddraw5)+(carddraw7))
+    else:
+        print(player1+str(-carddraw+carddraw3+carddraw5))
+
+if player200 =="stand":
+    print(player2+ str(-carddraw+carddraw3))
+else:
+    player2000 =input ("hit or stand")
+
+    if player2000 == "hit":
+        print ((player2)+str(carddraw2)+str(carddraw4)+str(carddraw6)+str(carddraw8))
+        print(carddraw2+carddraw4+carddraw6+carddraw8)
+    else:
+        print(player2+str(-carddraw2+carddraw4))
+
+    if player2000 == "stand":
+        print(player2+-carddraw2+carddraw4+carddraw6+carddraw8)
---
+++ felsökningslogg.md
@@ -0,0 +1,8 @@
+felsökningslogg.mg
+
+vad gick fel? 
+varför?
+hur löste jag det?
+vad skulle kag gjort annorlunda?
+
+Det som gick fel var att jag försökte att få mitt program att ignorera en annan del utan att sätta ihop delarna så de fungerade inte. men det gav bara fel resultat inte en error messege. Deta hände för att jag inte tänkte på att de var tvungna att sitta ihop jag trode att datorn förstog vad jag menade. Jag löste de genom att sätta ihop alla delarna så att jag kunnde få datorn att förstå vad som hände för att de jag skulle göra var förståt då jag fixade de. Det jag kunnat göra anorlunda är att jag hade kunnat tänka på att datorn är dum och inte förstår vad jag vill som AI kan så jag måste vara extremtydlig med vad jag säger till den
