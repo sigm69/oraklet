c448a6c1577121aa11ff28146eef435cf2aafdfd
Alexsander <alelin36@edu.umea.se>
2026-09-08 11:37:50 +0200

    lektion  8 sep tisdag. har inte en ai men ska fixa det

--- Slump Och Moduler.py
+++ Slump Och Moduler.py
@@ -6,51 +6,76 @@ carddraw = random.randint(1,11)
 carddraw3 = random.randint(1,11)
 carddraw5 = random.randint(1,11)
 carddraw7 = random.randint(1,11)
+carddraw9 = random.randint(1,11)
 print ((player1)+str("-")+ str(carddraw)+str(-carddraw3))
 print (carddraw+carddraw3)
 carddraw2 = random.randint(1,11)
 carddraw4 = random.randint(1,11)
 carddraw6 = random.randint(1,11)
 carddraw8 = random.randint(1,11)
+carddraw10 = random.randint(1,11)
 print((player2)+str("-")+ str(carddraw2)+str(-carddraw4))
 print(carddraw2+carddraw4)
 
-player100 = input("hit or stand")
+player100 = input(player1+"hit or stand-")
 if player100 == "hit": 
-    print (player1+str(carddraw5)+str(-carddraw)+str(-carddraw3))
-    print(carddraw+carddraw3+carddraw5)
+    print (player1+ "-du har korten"+str(-carddraw5)+str(-carddraw)+str(-carddraw3))
+    print(("vilket blir-")+str(carddraw+carddraw3+carddraw5))
 else: 
     print (player1+ str(-carddraw+carddraw3))
 
 
-player200 = input("hit or stand")
+player200 = input(player2+"hit or stand-")
 if player200 == "hit":
-    print(player2+str(carddraw6)+str(-carddraw2)+str(-carddraw4))
-    print(carddraw2+carddraw4+carddraw6)
+    print(player2+ "-du har korten"+str(-carddraw6)+str(-carddraw2)+str(-carddraw4))
+    print(("vilket blir-")+str(carddraw2+carddraw4+carddraw6))
 else:
     print(player2+str(-carddraw2+-carddraw4))
 
 if player100 == "stand":
     print(player1+ str(-carddraw+carddraw3))
 else:
-    player1000 = input ("hit or stand")
+    player1000 = input (player1+"-hit or stand")
 
     if player1000 == "hit":
         print (player1+ "-du har korten"+str(-carddraw)+str(-carddraw3)+str(-carddraw5)+str(-carddraw7))
-        print((carddraw)+(carddraw3)+(carddraw5)+(carddraw7))
+        print (("vilket blir-")+str(carddraw+carddraw3+carddraw5+carddraw7))
     else:
         print(player1+str(-carddraw+carddraw3+carddraw5))
 
 if player200 =="stand":
     print(player2+ str(-carddraw+carddraw3))
 else:
-    player2000 =input ("hit or stand")
+    player2000 =input (player2+"-hit or stand")
 
     if player2000 == "hit":
-        print ((player2)+str(carddraw2)+str(carddraw4)+str(carddraw6)+str(carddraw8))
-        print(carddraw2+carddraw4+carddraw6+carddraw8)
+        print ((player2)+ "-du har korten"+str(-carddraw2)+str(-carddraw4)+str(-carddraw6)+str(-carddraw8))
+        print(("vilket blir-")+str(carddraw2+carddraw4+carddraw6+carddraw8))
     else:
         print(player2+str(-carddraw2+carddraw4))
 
+if player100 == "stand":
+    print(" ")
+else: 
+    if player1000 == "stand":
+        print((-carddraw)+str(carddraw3)+str(carddraw5))
+    else: 
+        player10000 = input(player1+"hit or stand")
+        if player10000 == "hit":
+            print((player1)+ "-du har korten"+ str(-carddraw)+str(-carddraw3)+str(-carddraw5)+str(-carddraw7)+str(-carddraw9))
+            print("vilket blir-"+ str (carddraw+carddraw3+carddraw5+carddraw7+carddraw9))
+        else:
+            print(player1+ str(carddraw+carddraw3+carddraw5+carddraw7))
+
+if player200 == "stand":
+    print(" ")
+else: 
     if player2000 == "stand":
-        print(player2+-carddraw2+carddraw4+carddraw6+carddraw8)
+        print((-carddraw2)+str(carddraw4)+str(carddraw6))
+    else: 
+        player20000 = input(player2+"-hit or stand")
+        if player20000 == "hit":
+            print((player2)+ "-du har korten"+ str(-carddraw2)+str(-carddraw4)+str(-carddraw6)+str(-carddraw8)+str(-carddraw10))
+            print("vilket blir-"+ str (carddraw2+carddraw4+carddraw6+carddraw8+carddraw10))
+        else:
+            print(player2+ str(carddraw2+carddraw4+carddraw5+carddraw6))
